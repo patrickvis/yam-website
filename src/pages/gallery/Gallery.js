94c6e43e02075eb3491e26533ba0e765c12b6ede
MasonWang025 <mason.wang0025@gmail.com>
2020-07-19 13:57:47 -0700

    flexbox for gallery page started

--- src/pages/gallery/Gallery.js
+++ src/pages/gallery/Gallery.js
@@ -3,22 +3,34 @@ import React from "react";
 export default function Gallery() {
   return (
     <div className="gallery page">
-      <div className="d-none d-lg-block container-fluid m-0 p-0">
-        <div className="row">
-          {/* content begins here */}
-          {/* first column */}
-          <div className="col-5">
-            <div className="flex-container">sdf</div>
+      <div className="d-none d-md-flex content-container ">
+        {/* blm side */}
+        <div className="blm-column section">
+          <img
+            src={require("../../assets/img/gallery/blm-cover.jpg")}
+            alt="blm"
+            className="section-bg"
+          />
+          <h1>Black Lives Matter Gallery</h1>
+          <h2>Expressing justice through the arts</h2>
+        </div>
+        {/* rest */}
+        <div className="rest-column">
+          <div className="submit-row">submit</div>
+          <div className="art-sections">
+            <div className="row1">
+              <div className="visual-arts">arts</div>
+              <div className="creative-writing">write</div>
+            </div>
+            <div className=" row2">
+              <div className="photography">photo</div>
+              <div className="music">music</div>
+            </div>
           </div>
-          {/* second column */}
-          <div className="col-7">sfd</div>
-          {/* content ends here */}
         </div>
       </div>
-      <div className="d-none d-sm-block d-lg-none container-fluid m-0 p-0">
-        MEDIUM LAYOUT
-      </div>
-      <div className="d-block d-sm-none container-fluid m-0 p-0">
+
+      <div className="d-block d-md-none container-fluid m-0 p-0">
         SMALL LAYOUT
       </div>
     </div>

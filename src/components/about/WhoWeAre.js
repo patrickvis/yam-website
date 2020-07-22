518b645debbeef1eb0c1ab4d540893fa70ea223a
MasonWang025 <mason.wang0025@gmail.com>
2020-07-22 09:14:10 -0700

    abouts section draft 1 done

---
+++ src/components/about/WhoWeAre.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function WhoWeAre() {
+  return (
+    <div className="who-we-are text-center">
+      <div className="section-header">
+        <h1>Who are We?</h1>
+      </div>
+      <div className="content container">
+        <p className="lead-p">
+          We a <b>nonprofit</b> dedicated in serving <b>today's youth</b> and{" "}
+          <b>tomorrow's future.</b> <br />
+          The Youth Arts Movement is focused on{" "}
+          <b>providing a platform for your voice</b> through music, art,
+          literature, and all aspects of the arts.
+        </p>
+        <p>
+          Based in a community very much influenced in STEM, we saw the arts
+          develop a stigma of instability and unintelligence. We’ve all been
+          there, your poetry award or art prize never seemed to stack up against
+          a robotics championship. YAM strives to{" "}
+          <b>close this gap between the STEAM field and the arts</b>,
+          emphasizing the importance of both in our society today.
+        </p>
+        <p>
+          YAM provides students with opportunities to{" "}
+          <b>elevate their artwork and their own voices</b>. YAM competitions
+          provides winners with scholarship prizes, applicants with connections,
+          and communities with fundraisers and donations.
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Team from "../components/about/Team";
 import AboutHero from "../components/about/AboutHero";
+import WhoWeAre from "../components/about/WhoWeAre";
 
 export default function About() {
   return (
     <div className="about page">
       <AboutHero />
+      <WhoWeAre />
       <Team />
     </div>
   );

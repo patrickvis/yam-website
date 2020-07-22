42f43a9c8f86065483c127153809f739b3dfead6
MasonWang025 <mason.wang0025@gmail.com>
2020-07-22 08:47:09 -0700

    about hero

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Team from "../components/about/Team";
+import AboutHero from "../components/about/AboutHero";
 
 export default function About() {
   return (
     <div className="about page">
+      <AboutHero />
       <Team />
     </div>
   );

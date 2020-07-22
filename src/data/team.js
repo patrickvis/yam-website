da9b5919c70f5542bb7128942efbd911ee21d470
MasonWang025 <mason.wang0025@gmail.com>
2020-07-22 16:02:22 -0700

    refactor into new data folder

---
+++ src/data/team.js
@@ -0,0 +1,49 @@
+const teamMembers = [
+  {
+    name: "Brook Chuang",
+    position: "Co-founder and COO",
+    linkedin: "https://www.linkedin.com/in/chuangbrook ",
+  },
+  {
+    name: "Jonathan Li",
+    position: "Co-founder and CFO",
+    instagram: "https://www.instagram.com/jonathan_jli/",
+    facebook: "https://www.facebook.com/jonathan.li.758399",
+  },
+  {
+    name: "Isabella Lee",
+    position: "Program VP",
+    instagram: "https://www.instagram.com/isabella.lee13/",
+  },
+  {
+    name: "Mason Wang",
+    position: "Tech Director and Webmaster",
+    linkedin: "https://www.linkedin.com/in/mason-wang-7b2758176/",
+    github: "https://github.com/MasonWang025",
+    instagram: "https://www.instagram.com/masonwang025/",
+  },
+  {
+    name: "Charlie Williams",
+    position: "CMO",
+    linkedin: "https://linkedin.com/in/charlie-williams-1bb4021b3",
+  },
+
+  {
+    name: "Katie Chen",
+    position: "Outreach and West Coast Branch President",
+    linkedin: "https://www.linkedin.com/in/katie-chen-3a8b83177/",
+    instagram: "https://www.instagram.com/katiechnn/",
+  },
+  {
+    name: "Tiffany Lieu",
+    position: "Marketing Manager",
+    linkedin: "https://instagram.com/tfaffniy?igshid=ywfykngjeirx",
+  },
+  {
+    name: "Diana Le",
+    position: "Graphic Designer",
+    linkedin: "https://www.instagram.com/elkanaid/",
+  },
+];
+
+export default teamMembers;

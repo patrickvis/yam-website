1f5779389a5a8f2f052a90b7ebb05142b13d47bd
MasonWang025 <mason.wang0025@gmail.com>
2020-07-24 17:15:15 -0700

    guidelines fixed

--- src/components/gallery/submit/Guidelines.js
+++ src/components/gallery/submit/Guidelines.js
@@ -27,20 +27,25 @@ export default function Guidelines() {
       </div>
       <div className="container text-center">
         <div className="row pb-5 justify-content-center">
-          <div className="col-12 mb-2">
+          <div className="col-12 mb-3">
             <p className="lead-p">
               <b>No purchase necessary</b> to enter.
               <br />
               Rules that do not follow guidelines <b>will not be shown</b>.
             </p>
           </div>
-          <div className="col-11 col-md-9 text-md-left text-center">
-            {rules.map((rule, index) => (
-              <p className="" key={index}>
-                <span className="number">{index + 1}. </span> {rule}
-                <br />
-              </p>
-            ))}
+          <div className="col-11 col-md-9   rules text-md-left text-center">
+            <div className="container-fluid p-0 m-0">
+              {rules.map((rule, index) => (
+                <div className="row no-gutters p-0 m-0" key={index}>
+                  <div className="col-md-1 text-md-center mt-3 mt-sm-2 mt-md-0">
+                    <span className="number">{index + 1}. </span>
+                  </div>
+                  <div className="col-md-11 col-12 text-center">{rule}</div>
+                  <div className="w-100 mb-4 mb-md-3"></div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>

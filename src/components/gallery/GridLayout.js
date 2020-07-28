037521cd876464baa78b69cbf524777f135b4083
MasonWang025 <mason.wang0025@gmail.com>
2020-07-28 11:43:15 -0700

    search bar added

--- src/components/gallery/GridLayout.js
+++ src/components/gallery/GridLayout.js
@@ -4,33 +4,39 @@ import { Link } from "react-router-dom";
 
 export default function GridLayout(props) {
   return (
-    <div className="grid-layout container align-items-center px-1">
-      <div className="row no-gutters gallery-row justify-content-center">
-        {props.images.map((img) => {
-          let id = /[^/]*$/.exec(img.imageURL)[0];
-          return (
-            <div
-              className="col-12 col-sm-6 col-md-4 col-lg-3 d-flex align-items-center img-col"
-              id={id}
-              key={id}
-            >
-              <div className="wrapper d-flex flex-column align-items-stretch text-center">
-                <Link>
-                  <Image
-                    cloudName="masonwang"
-                    publicId={img.imageURL}
-                    className="img-fluid gallery-image"
-                    style={{ backgroundColor: "#152828" }}
-                  />
-                </Link>
-                <div className="title">
-                  <i>{img.title}</i> by <b className="theme">{img.artist}</b>
+    <div className="grid-layout container align-items-center px-1 pt-3 pb-5">
+      {props.images.length > 0 ? (
+        <div className="row no-gutters gallery-row justify-content-center">
+          {props.images.map((img) => {
+            let id = /[^/]*$/.exec(img.imageURL)[0];
+            return (
+              <div
+                className="col-12 col-sm-6 col-md-4 col-lg-3 d-flex align-items-center img-col"
+                id={id}
+                key={id}
+              >
+                <div className="wrapper d-flex flex-column align-items-stretch text-center">
+                  <Link to="/gallery/visualarts">
+                    <Image
+                      cloudName="masonwang"
+                      publicId={img.imageURL}
+                      className="img-fluid gallery-image"
+                      style={{ backgroundColor: "#152828" }}
+                    />
+                  </Link>
+                  <div className="title">
+                    <i>{img.title}</i> by <b className="theme">{img.artist}</b>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      ) : (
+        <p className="text-center pt-3">
+          Sorry, there are no results matching your search.
+        </p>
+      )}
     </div>
   );
 }
--- src/pages/gallery/VisualArts.js
+++ src/pages/gallery/VisualArts.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import GridLayout from "../../components/gallery/GridLayout";
 import SectionHero from "../../components/SectionHero";
 import { visualarts } from "../../data/galleries";
 
 export default function VisualArts() {
+  const [images, setImages] = useState(visualarts);
+
   return (
     <div className="page">
       <SectionHero
@@ -11,7 +13,26 @@ export default function VisualArts() {
         subtitle="YAM Visual Arts Gallery"
         imageURL="https://res.cloudinary.com/masonwang/image/upload/v1595458581/yam-website/gallery/previews/visualarts-cover.jpg"
       />
-      <GridLayout images={visualarts} />
+      <div className="container filter-grid pt-5">
+        <input
+          type="text"
+          placeholder="Search by title or artist"
+          onChange={(e) => {
+            const filterText = e.target.value.toLowerCase();
+            setImages(
+              visualarts.filter((image) => {
+                if (
+                  image.title.toLowerCase().includes(filterText) ||
+                  image.artist.toLowerCase().includes(filterText)
+                )
+                  return true;
+                return false;
+              })
+            );
+          }}
+        />
+      </div>
+      <GridLayout images={images} />
     </div>
   );
 }

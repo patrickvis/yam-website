8e67325d61482e3ea2971f5ac83f37edf2d76cac
MasonWang025 <mason.wang0025@gmail.com>
2020-07-28 12:24:51 -0700

    layout with equal size images

--- src/components/gallery/GridLayout.js
+++ src/components/gallery/GridLayout.js
@@ -11,29 +11,32 @@ export default function GridLayout(props) {
             let id = /[^/]*$/.exec(img.imageURL)[0];
             return (
               <div
-                className="col-12 col-sm-6 col-md-4 col-lg-3 d-flex align-items-center img-col"
+                className="col-12 col-sm-6 col-md-4 col-lg-3 d-flex align-items-stretch img-col"
                 id={id}
                 key={id}
               >
-                <div className="wrapper d-flex flex-column align-items-stretch text-center">
-                  <Link to="/gallery/visualarts">
-                    <Image
-                      cloudName="masonwang"
-                      publicId={img.imageURL}
-                      className="img-fluid gallery-image"
-                      style={{ backgroundColor: "#152828" }}
-                    />
-                  </Link>
+                <Link
+                  to="/gallery/visualarts"
+                  className="d-flex align-items-stretch flex-row"
+                >
+                  <Image
+                    cloudName="masonwang"
+                    publicId={img.imageURL}
+                    className="img-fluid gallery-image"
+                    style={{ backgroundColor: "#152828" }}
+                  />
                   <div className="title">
-                    <i>{img.title}</i> by <b className="theme">{img.artist}</b>
+                    <span>
+                      <i>{img.title}</i> by <b>{img.artist}</b>
+                    </span>
                   </div>
-                </div>
+                </Link>
               </div>
             );
           })}
         </div>
       ) : (
-        <p className="text-center pt-3">
+        <p className="text-center pt-5">
           Sorry, there are no results matching your search.
         </p>
       )}

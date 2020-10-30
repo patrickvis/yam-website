cf95fb6fd938cbb570cc4f93c24d5b69134ee413
Shreyas Rana <ranashreyas@gmail.com>
2020-10-29 22:28:49 -0700

    about

--- src/data/team.js
+++ src/data/team.js
@@ -1,98 +1,129 @@
 const teamMembers = [
-  {
-    name: "Brook Chuang",
-    position: "Co-founder and COO",
-    linkedin: "https://www.linkedin.com/in/chuangbrook ",
-  },
-  {
-    name: "Jonathan Li",
-    position: "Co-founder and CFO",
-    instagram: "https://www.instagram.com/jonathan_jli/",
-    facebook: "https://www.facebook.com/jonathan.li.758399",
-  },
-  {
-    name: "Isabella Lee",
-    position: "CNO",
-    instagram: "https://www.instagram.com/isabella.lee13/",
-  },
-  {
-    name: "Ethan Lin",
-    position: "Secretary",
-    instagram: "https://www.instagram.com/ethan._lin/",
-  },
-  {
-    name: "Mason Wang",
-    position: "Tech President and Webmaster",
-    linkedin: "https://www.linkedin.com/in/mason-wang-7b2758176/",
-    github: "https://github.com/MasonWang025",
-    instagram: "https://www.instagram.com/masonwang025/",
-  },
-  {
-    name: "Charlie Williams",
-    position: "CMO",
-    linkedin: "https://linkedin.com/in/charlie-williams-1bb4021b3",
-  },
-  {
-    name: "Linda Zhao",
-    position: "Graphics President",
-    instagram: "https://instagram.com/lindazhqo?igshid=18k3rxzoorff0",
-    linkedin: "http://linkedin.com/in/linda-zhao-4250361b3 ",
-    imageURL:
-      "https://res.cloudinary.com/masonwang/image/upload/v1595422647/yam-website/team/linda-zhao.jpg",
-  },
-  {
-    name: "Grace Kuo",
-    position: "Program Manager",
-    instagram: "https://www.instagram.com/gwaciek/",
-    instagram2: "https://www.instagram.com/behrylium/",
-    imageURL:
-      "https://res.cloudinary.com/masonwang/image/upload/v1596132782/yam-website/team/grace-kuo.jpg",
-  },
-  {
-    name: "Nalin Chopra",
-    position: "Sponsorship Manager",
-    linkedin: "https://www.linkedin.com/in/nalinchopra",
-    imageURL:
-      "https://res.cloudinary.com/masonwang/image/upload/v1596133128/yam-website/team/nalin-chopra.jpg",
-  },
-  {
-    name: "Katie Chen",
-    position: "Outreach and West Coast Branch President",
-    linkedin: "https://www.linkedin.com/in/katie-chen-3a8b83177/",
-    instagram: "https://www.instagram.com/katiechnn/",
-  },
-  {
-    name: "Tiffany Lieu",
-    position: "Marketing Manager",
-    linkedin: "https://wwww.instagram.com/tfaffniy?igshid=ywfykngjeirx",
-  },
-  {
-    name: "Nandini Desai",
-    position: "Expansion Officer",
-    linkedin: "https://www.instagram.com/nandini.des/",
-    instagram: "https://www.linkedin.com/in/nandini-desai-b712331a1",
-    imageURL:
-      "https://res.cloudinary.com/masonwang/image/upload/v1596133059/yam-website/team/nandini-desai_xfqqoy.jpg",
-  },
-  {
-    name: "Joanna Lu",
-    position: "Expansion Officer",
-    linkedin: "https://www.linkedin.com/in/joanna-lu-a85a6a196/ ",
-    imageURL:
-      "https://res.cloudinary.com/masonwang/image/upload/v1596133353/yam-website/team/joanna-lu_cub5fm.jpg",
-  },
-  {
-    name: "Diana Le",
-    position: "Graphic Designer",
-    linkedin: "https://www.instagram.com/elkanaid/",
-  },
-  {
-    name: "Carolann Dong",
-    position: "Graphic Designer",
-    linkedin: "https://www.instagram.com/carolanndong/",
-    imageURL:
-      "https://res.cloudinary.com/masonwang/image/upload/v1597014960/yam-website/team/carolann-dong.jpg",
-  },
+    {
+      name: "Brook Chuang",
+      position: "Co-founder and COO",
+      linkedin: "https://www.linkedin.com/in/chuangbrook ",
+    },
+    {
+      name: "Jonathan Li",
+      position: "Co-founder and CFO",
+      instagram: "https://www.instagram.com/jonathan_jli/",
+      facebook: "https://www.facebook.com/jonathan.li.758399",
+    },
+    {
+      name: "Isabella Lee",
+      position: "CNO",
+      instagram: "https://www.instagram.com/isabella.lee13/",
+    },
+    {
+      name: "Ethan Lin",
+      position: "Secretary",
+      instagram: "https://www.instagram.com/ethan._lin/",
+    },
+    {
+      name: "Shreyas Rana",
+      position: "Tech President and Webmaster",
+      instagram: "https://www.instagram.com/_shreyasrana_/",
+      linkedin: "https://www.linkedin.com/in/shreyas-rana-61b0a5156/",
+      github: "https://github.com/ranashreyas",
+      imageURL:
+        "https://res.cloudinary.com/dmpjsarum/image/upload/v1604035482/Team/ShreyasRanaHeadShot_azemsl.jpg",
+    },
+    {
+      name: "Charlie Williams",
+      position: "CMO",
+      linkedin: "https://linkedin.com/in/charlie-williams-1bb4021b3",
+    },
+    {
+      name: "Linda Zhao",
+      position: "Graphics President",
+      instagram: "https://instagram.com/lindazhqo?igshid=18k3rxzoorff0",
+      linkedin: "http://linkedin.com/in/linda-zhao-4250361b3 ",
+      imageURL:
+        "https://res.cloudinary.com/masonwang/image/upload/v1595422647/yam-website/team/linda-zhao.jpg",
+    },
+    {
+      name: "Grace Kuo",
+      position: "Program Manager",
+      instagram: "https://www.instagram.com/gwaciek/",
+      instagram2: "https://www.instagram.com/behrylium/",
+      imageURL:
+        "https://res.cloudinary.com/masonwang/image/upload/v1596132782/yam-website/team/grace-kuo.jpg",
+    },
+    {
+      name: "Kai Doemling",
+      position: "YAM Ambassador",
+      instagram: "https://www.instagram.com/kaidoem/",
+      imageURL:
+        "https://res.cloudinary.com/dmpjsarum/image/upload/v1604034921/Team/Screen_Shot_2020-10-29_at_10.14.57_PM_fgay1z.png",
+    },
+    {
+      name: "Grace Meyer",
+      position: "Grant Manager",
+      instagram: "https://www.instagram.com/gracee.aem/",
+      imageURL:
+        "https://res.cloudinary.com/dmpjsarum/image/upload/v1604034567/Team/Screen_Shot_2020-10-29_at_10.09.06_PM_c1wrgf.png",
+    },
+    {
+      name: "Nalin Chopra",
+      position: "Sponsorship Manager",
+      linkedin: "https://www.linkedin.com/in/nalinchopra",
+      imageURL:
+        "https://res.cloudinary.com/masonwang/image/upload/v1596133128/yam-website/team/nalin-chopra.jpg",
+    },
+  // {
+  //   name: "Katie Chen",
+  //   position: "Outreach and West Coast Branch President",
+  //   linkedin: "https://www.linkedin.com/in/katie-chen-3a8b83177/",
+  //   instagram: "https://www.instagram.com/katiechnn/",
+  // },
+    {
+      name: "Tiffany Lieu",
+      position: "Marketing Manager",
+      instagram: "https://wwww.instagram.com/tfaffniy?igshid=ywfykngjeirx",
+    },
+    {
+      name: "Rhea Werner",
+      position: "Australia Branch President",
+      linkedin: "https://www.linkedin.com/in/rhea-werner-7606621b1/",
+      instagram: "https://www.instagram.com/rhea_wernerr/",
+      imageURL:
+        "https://res.cloudinary.com/dmpjsarum/image/upload/v1604034316/Team/Screen_Shot_2020-10-29_at_10.04.51_PM_moxuf5.png",
+    },
+    {
+      name: "Nandini Desai",
+      position: "Expansion Officer",
+      linkedin: "https://www.linkedin.com/in/nandini-desai-b712331a1",
+      instagram: "https://www.instagram.com/nandini.des/",
+      imageURL:
+        "https://res.cloudinary.com/dmpjsarum/image/upload/c_scale,h_420,w_420/v1604033736/Team/Screen_Shot_2020-10-29_at_9.54.58_PM_zncmqh.png",
+    },
+    {
+      name: "Joanna Lu",
+      position: "Expansion Officer",
+      linkedin: "https://www.linkedin.com/in/joanna-lu-a85a6a196/ ",
+      imageURL:
+        "https://res.cloudinary.com/masonwang/image/upload/v1596133353/yam-website/team/joanna-lu_cub5fm.jpg",
+    },
+  // {
+  //   name: "Diana Le",
+  //   position: "Graphic Designer",
+  //   linkedin: "https://www.instagram.com/elkanaid/",
+  // },
+    {
+      name: "Carolann Dong",
+      position: "Graphic Designer",
+      instagram: "https://www.instagram.com/carolanndong/",
+      imageURL:
+        "https://res.cloudinary.com/masonwang/image/upload/v1597014960/yam-website/team/carolann-dong.jpg",
+    },
+    {
+      name: "Hannah Su",
+      position: "Outreach",
+      linkedin: "https://www.linkedin.com/in/hannah-s-62402b1a4/",
+      imageURL:
+        "https://res.cloudinary.com/dmpjsarum/image/upload/v1604035069/Team/Screen_Shot_2020-10-29_at_10.17.31_PM_kxx5wa.png",
+    },
 ];
 
 export default teamMembers;
--- src/pages/Chapters.js
+++ src/pages/Chapters.js
@@ -10,7 +10,8 @@ export default function Chapters() {
       <SectionHero
         title="Chapters"
         subtitle="Register a YAM Chapter"
-        imageURL="https://res.cloudinary.com/masonwang/image/upload/v1595541747/yam-website/gallery/previews/about-us-cover.jpg"
+        // imageURL="https://res.cloudinary.com/masonwang/image/upload/v1595541747/yam-website/gallery/previews/about-us-cover.jpg"
+        imageURL="https://res.cloudinary.com/dmpjsarum/image/upload/v1603608876/image0_krzgoj.jpg"
       />
       <WhyChapters />
       <OurChapters />

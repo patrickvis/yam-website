ad70da602b2ba41840d23037a4eb522cfe7dd63e
Shreyas Rana <ranashreyas@gmail.com>
2021-01-08 22:46:50 -0800

    slideshowgit add *

--- src/components/projects/CurrentProjects.js
+++ src/components/projects/CurrentProjects.js
@@ -36,67 +36,25 @@ export default function CurrentProjects() {
               </div>
             </div>
             
-            <section class="carousel" aria-label="Gallery">
-              <ol class="carousel__viewport">
-                <li id="carousel__slide1"
-                    tabindex="0"
-                    class="carousel__slide">
-                  <div class="carousel__snapper">
-                    <a href="#carousel__slide4"
-                      class="carousel__prev">Go to last slide</a>
-                    <a href="#carousel__slide2"
-                      class="carousel__next">Go to next slide</a>
-                  </div>
-                </li>
-                <li id="carousel__slide2"
-                    tabindex="0"
-                    class="carousel__slide">
-                  <div class="carousel__snapper"></div>
-                  <a href="#carousel__slide1"
-                    class="carousel__prev">Go to previous slide</a>
-                  <a href="#carousel__slide3"
-                    class="carousel__next">Go to next slide</a>
-                </li>
-                <li id="carousel__slide3"
-                    tabindex="0"
-                    class="carousel__slide">
-                  <div class="carousel__snapper"></div>
-                  <a href="#carousel__slide2"
-                    class="carousel__prev">Go to previous slide</a>
-                  <a href="#carousel__slide4"
-                    class="carousel__next">Go to next slide</a>
-                </li>
-                <li id="carousel__slide4"
-                    tabindex="0"
-                    class="carousel__slide">
-                  <div class="carousel__snapper"></div>
-                  <a href="#carousel__slide3"
-                    class="carousel__prev">Go to previous slide</a>
-                  <a href="#carousel__slide1"
-                    class="carousel__next">Go to first slide</a>
-                </li>
-              </ol>
-              <aside class="carousel__navigation">
-                <ol class="carousel__navigation-list">
-                  <li class="carousel__navigation-item">
-                    <a href="#carousel__slide1"
-                      class="carousel__navigation-button">Go to slide 1</a>
-                  </li>
-                  <li class="carousel__navigation-item">
-                    <a href="#carousel__slide2"
-                      class="carousel__navigation-button">Go to slide 2</a>
-                  </li>
-                  <li class="carousel__navigation-item">
-                    <a href="#carousel__slide3"
-                      class="carousel__navigation-button">Go to slide 3</a>
-                  </li>
-                  <li class="carousel__navigation-item">
-                    <a href="#carousel__slide4"
-                      class="carousel__navigation-button">Go to slide 4</a>
-                  </li>
-                </ol>
-              </aside>
-            </section>
+            <div id="slideshow"> 
+              <div class="slide-wrapper"> 
+                <div class="slide"> 
+                  <img src={require("../../assets/Projects/AMASlides/pic1.png")} alt="" class = 'slideImg'></img> 
+                </div> 
+                <div class="slide"> 
+                <img src={require("../../assets/Projects/AMASlides/pic2.png")} alt="" class = 'slideImg'></img> 
+                </div> 
+                <div class="slide"> 
+                <img src={require("../../assets/Projects/AMASlides/pic3.png")} alt="" class = 'slideImg'></img> 
+                </div> 
+                <div class="slide"> 
+                <img src={require("../../assets/Projects/AMASlides/pic4.png")} alt="" class = 'slideImg'></img> 
+                </div> 
+              </div> 
+            </div>
+
+
+
           </div>
         </div>
       </div>

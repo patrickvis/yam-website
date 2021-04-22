4be9ba2375fbb2f1db1e64c2fc0346da887e6689
nihkee <75652138+nihkee@users.noreply.github.com>
2021-04-21 23:38:44 -0400

    Update Speakers.js

--- src/components/projects/Speakers.js
+++ src/components/projects/Speakers.js
@@ -62,7 +62,7 @@ export default function Speakers() {
             <div class="container">
                 <h4><b>Jeff Martin</b></h4>
                 <a
-                        href="https://www.writerstrust.com/authors/greg-brown/"
+                        href="https://www.theyoungwriters.org/artists/jeff-martin"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="action-btn mx-2 mb-2"
@@ -77,22 +77,7 @@ export default function Speakers() {
             <div class="container">
                 <h4><b>Ryan Williams</b></h4>
                 <a
-                        href="https://www.writerstrust.com/authors/greg-brown/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="action-btn mx-2 mb-2"
-                    >
-                        Read More
-                </a>
-            </div>
-            </div>
-
-            <div class="card">
-            <img src={require("../../assets/Projects/speakers/StaceyMatson.png")} alt="location" style={{width:"100%"}}></img>
-            <div class="container">
-                <h4><b>Stacey Matson</b></h4>
-                <a
-                        href="https://www.writerstrust.com/authors/greg-brown/"
+                        href="https://www.saatchiart.com/Xintalene"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="action-btn mx-2 mb-2"

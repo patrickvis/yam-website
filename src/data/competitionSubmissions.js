4044906bfa7f947f50cfbbb8b4b684ffaa44f0e2
nihkee <75652138+nihkee@users.noreply.github.com>
2021-08-04 23:49:00 -0400

    Update and rename competitionSubs.js to competitionSubmissions.js

---
+++ src/data/competitionSubmissions.js
@@ -0,0 +1,239 @@
+const competitionSubmissions = [
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131552/yamCompetesSubmissions/Anica%20Liu.webp",
+    artist: "Anica Liu",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131413/yamCompetesSubmissions/KathrynBediako.webp",
+    artist: "Kathryn Bediako",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131287/yamCompetesSubmissions/JessieZhu.webp",
+    artist: "Jessie Zhu",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131586/yamCompetesSubmissions/SophiaKagan1.webp",
+    artist: "Sophia Kagan",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131856/yamCompetesSubmissions/IrisLiu.webp",
+    artist: "Iris Liu",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131949/yamCompetesSubmissions/KaileyHu.webp",
+    artist: "Kailey Hu",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131773/yamCompetesSubmissions/KaelynnTan.webp",
+    artist: "Kaelynn Tan",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628130435/yamCompetesSubmissions/lindaWong.webp",
+    artist: "Linda Wong",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628130854/yamCompetesSubmissions/nehaVedaprahash.webp",
+    artist: "Neha Vedaprahash",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628130904/yamCompetesSubmissions/KaushikTalur.webp",
+    artist: "Kaushik Talur",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628130985/yamCompetesSubmissions/KaushikTalur2.webp",
+    artist: "Kaushik Talur",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131014/yamCompetesSubmissions/KaushikTalur3.webp",
+    artist: "Kaushik Talur",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131049/yamCompetesSubmissions/KaushikTalur4.webp",
+    artist: "Kaushik Talur",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131075/yamCompetesSubmissions/KaushikTalur5.webp",
+    artist: "Kaushik Talur",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131107/yamCompetesSubmissions/SravyaNagalakunta.webp",
+    artist: "Sravya Nagalakunta",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131221/yamCompetesSubmissions/Amanda.webp",
+    artist: "Amanda",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131248/yamCompetesSubmissions/Eyl%C3%BCl%C3%87elikk%C4%B1ran.webp",
+    artist: "Eylül Çelikkıran",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131321/yamCompetesSubmissions/CindyZhou.webp",
+    artist: "Cindy Zhou",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131353/yamCompetesSubmissions/HansenTao1.webp",
+    artist: "Hansen Tao",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131381/yamCompetesSubmissions/HansenTao2.webp",
+    artist: "HansenTao",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131448/yamCompetesSubmissions/LorenPante.webp",
+    artist: "Loren Pante",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131478/yamCompetesSubmissions/JadeSupnet.webp",
+    artist: "Jade Supnet",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131515/yamCompetesSubmissions/AnoushkaGarg.webp",
+    artist: "Anoushka Garg",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131620/yamCompetesSubmissions/SophiaKagan2.webp",
+    artist: "Sophia Kagan",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131656/yamCompetesSubmissions/SheenaLai.webp",
+    artist: "Sheena Lai",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131697/yamCompetesSubmissions/JasmineSerna.webp",
+    artist: "Jasmine Serna",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131735/yamCompetesSubmissions/SwayambaraSarkar.webp",
+    artist: "Swayambara Sarkar",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131812/yamCompetesSubmissions/Simone%22Mo%22Koelle.webp",
+    artist: "Simone Mo Koelle",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131900/yamCompetesSubmissions/ElizabethZheng.webp",
+    artist: "Elizabeth Zheng",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628131990/yamCompetesSubmissions/NavyaDesai.webp",
+    artist: "Navya Desai",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628132030/yamCompetesSubmissions/SanaJahangir1.webp",
+    artist: "Sana Jahangir",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628132084/yamCompetesSubmissions/SanaJahangir2.webp",
+    artist: "Sana Jahangir",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628132127/yamCompetesSubmissions/SophieChen.webp",
+    artist: "Sophie Chen",
+    title: "",
+    description: "",
+  },
+  
+  {
+    imageURL: "https://res.cloudinary.com/nikkic/image/upload/v1628132170/yamCompetesSubmissions/JulianChristianSimmons.webp",
+    artist: "Julian Christian Simmons",
+    title: "",
+    description: "",
+  },
+  

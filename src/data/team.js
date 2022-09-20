d42f2332b6001f0680f1ea93bb0191e103559660
Aaryahi Vaidya <aaryahi.v@gmail.com>
2022-09-19 21:52:51 -0700

    Delete team member from team.js

--- src/data/team.js
+++ src/data/team.js
@@ -8,13 +8,6 @@ const teamMembers = [
       "https://res.cloudinary.com/masonwang/image/upload/v1595422647/yam-website/team/linda-zhao.jpg",
   },
   {
-    name: "Jonathan Li",
-    position: "Co-founder and CFO",
-    instagram: "https://www.instagram.com/jonathan_jli/",
-    facebook: "https://www.facebook.com/jonathan.li.758399",
-    imageURL: "https://res.cloudinary.com/yam18/image/upload/v1619581237/YAM%20TEAM/Jonathan_HeadShot_xfd9ce.jpg"
-  },
-  {
     name: "Isabella Lee",
     position: "CNO",
     instagram: "https://www.instagram.com/isabella.lee13/",

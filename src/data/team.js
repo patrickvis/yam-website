06c551c8cee282d377f7d265bee540c55af2ec75
Aaryahi Vaidya <aaryahi.v@gmail.com>
2022-09-19 22:35:50 -0700

    Delete prior team members

--- src/data/team.js
+++ src/data/team.js
@@ -8,16 +8,6 @@ const teamMembers = [
       "https://res.cloudinary.com/masonwang/image/upload/v1595422647/yam-website/team/linda-zhao.jpg",
   },
   {
-    name: "Isabella Lee",
-    position: "CNO",
-    instagram: "https://www.instagram.com/isabella.lee13/",
-  },
-  {
-    name: "Ethan Lin",
-    position: "Secretary",
-    instagram: "https://www.instagram.com/ethan._lin/",
-  },
-  {
     name: "Diana Le",
     position: "Graphic Designer",
     instagram: "https://www.instagram.com/elkanaid/",
@@ -26,23 +16,9 @@ const teamMembers = [
       "https://res.cloudinary.com/yam18/image/upload/v1620094767/YAM%20TEAM/Diana_Headshot_fjpab9.jpg",
   },
   {
-    name: "Kai Doemling",
-    position: "Outreach President",
-    instagram: "https://www.instagram.com/kaidoem/",
-    imageURL:
-      "https://res.cloudinary.com/dmpjsarum/image/upload/v1604034921/Team/Screen_Shot_2020-10-29_at_10.14.57_PM_fgay1z.png",
-  },
-  {
     name: "Tiffany Lieu",
     position: "CMO",
     instagram: "https://wwww.instagram.com/tfaffniy?igshid=ywfykngjeirx",
-  },
-  {
-    name: "Carolann Dong",
-    position: "Graphic Designer",
-    instagram: "https://www.instagram.com/carolanndong/",
-    imageURL:
-      "https://res.cloudinary.com/masonwang/image/upload/v1597014960/yam-website/team/carolann-dong.jpg",
   },
   {
     name: "Dylan Gee",
@@ -55,13 +31,6 @@ const teamMembers = [
     position: "Marketing Officer",
     instagram: "https://www.instagram.com/anushka.tadikonda/",
     imageURL: require("../assets/team/anushkatadikonda.png")
-  },
-
-  {
-    name: "Braisly Varg",
-    position: "Outreach Officer",
-    instagram: "https://www.instagram.com/braislyvarg/",
-    imageURL: require("../assets/team/braislyvarg.png"),
   },
   {
     name: "Hailey Luong",

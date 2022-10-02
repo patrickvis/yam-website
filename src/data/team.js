53c66a44d5a7964148d9108e3fb4929b6bc6eedb
Aaryahi Vaidya <aaryahi.v@gmail.com>
2022-10-01 19:21:16 -0700

    Update team.js

--- src/data/team.js
+++ src/data/team.js
@@ -1,43 +1,64 @@
 const teamMembers = [
-  {
+   {
     name: "Linda Zhao",
     position: "interim-CEO",
-    instagram: "https://instagram.com/lindazhqo?igshid=18k3rxzoorff0",
+    instagram: "https://www.instagram.com/lindazhqo/",
     linkedin: "http://linkedin.com/in/linda-zhao-4250361b3 ",
-    imageURL:
-      "https://res.cloudinary.com/masonwang/image/upload/v1595422647/yam-website/team/linda-zhao.jpg",
-  },
-  {
+    imageURL: require("../assets/team/lindazhao.png")
+   },
+{
     name: "Diana Le",
     position: "Graphic Designer",
     instagram: "https://www.instagram.com/elkanaid/",
     linkedin: "http://www.linkedin.com/in/dianakle",
-    imageURL:
-      "https://res.cloudinary.com/yam18/image/upload/v1620094767/YAM%20TEAM/Diana_Headshot_fjpab9.jpg",
+    imageURL: require("../assets/team/dianale.png")
   },
   {
-    name: "Tiffany Lieu",
-    position: "CMO",
-    instagram: "https://wwww.instagram.com/tfaffniy?igshid=ywfykngjeirx",
+   name: "Tiffany Lieu",
+   position: "CMO",
+   instagram: "https://www.instagram.com/tfaffniy/",
+   imageURL: require("../assets/team/tiffanylieu.png")
   },
   {
     name: "Dylan Gee",
-    position: "Marketing Officer",
+    position: "Finance Officer",
     instagram: "https://www.instagram.com/dyland_gee/",
     imageURL: require("../assets/team/dylandgee.png")
   },
   {
     name: "Anushka Tadikonda",
-    position: "Marketing Officer",
+    position: "Marketing & Outreach Officer",
     instagram: "https://www.instagram.com/anushka.tadikonda/",
     imageURL: require("../assets/team/anushkatadikonda.png")
   },
+
   {
     name: "Hailey Luong",
     position: "Social Media Manager",
     instagram: "https://www.instagram.com/haileyxluong/",
     imageURL: require("../assets/team/haileyxluong.png")
   },
+  {
+    name: "Aaryahi Vaidya",
+    position: "CTO",
+    instagram: “https://www.instagram.com/aari.vaidya/",
+    github: “https://github.com/aaryahiv?tab=repositories”,
+    linkedin: “https://www.linkedin.com/in/aaryahi-vaidya-110553242”,
+    imageURL: require("../assets/team/aarivaidya.png")
+  },
+  {
+    name: "Paulina Depaulo",
+    position: "Finance Officer",
+    instagram: “https://www.instagram.com/paulina.maee",
+    imageURL: require("../assets/team/paulinadepaulo.png")
+  },
+  {
+    name: "David Rendon",
+    position: "Secretary",
+    instagram: “https://www.instagram.com/paulina.maee",
+    imageURL: require("../assets/team/davidrendon.png")
+  }
+
 ];
 
 export default teamMembers;

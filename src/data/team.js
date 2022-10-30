ac7101b9bbbe5b88a3fd6e6bed6bdc75a84d6eac
caliwolv <112672667+caliwolv@users.noreply.github.com>
2022-10-30 00:02:21 -0700

    Update team.js
    More fixes

--- src/data/team.js
+++ src/data/team.js
@@ -11,32 +11,33 @@ const teamMembers = [
     position: "Graphic Designer",
     instagram: "https://www.instagram.com/elkanaid/",
     linkedin: "http://www.linkedin.com/in/dianakle",
-    imageURL: https://user-images.githubusercontent.com/22718430/196056813-d4c0679b-2bea-4c5d-b348-4ffca10128d8.png
+    imageURL: "https://user-images.githubusercontent.com/22718430/196056813-d4c0679b-2bea-4c5d-b348-4ffca10128d8.png",
+  
   },
   {
    name: "Tiffany Lieu",
    position: "CMO",
    instagram: "https://www.instagram.com/tfaffniy/",
-   imageURL: https://user-images.githubusercontent.com/22718430/196056918-e26b94d9-6a18-4a3d-bbda-920e6d79885b.png
+   imageURL: "https://user-images.githubusercontent.com/22718430/196056918-e26b94d9-6a18-4a3d-bbda-920e6d79885b.png",
   },
   {
     name: "Dylan Gee",
     position: "Finance Officer",
     instagram: "https://www.instagram.com/dyland_gee/",
-    imageURL: https://user-images.githubusercontent.com/22718430/196056831-b66152f8-47f7-4b15-ae83-9285d85129b1.png
+    imageURL: "https://user-images.githubusercontent.com/22718430/196056831-b66152f8-47f7-4b15-ae83-9285d85129b1.png",
   },
   {
     name: "Anushka Tadikonda",
     position: "Marketing & Outreach Officer",
     instagram: "https://www.instagram.com/anushka.tadikonda/",
-    imageURL: https://user-images.githubusercontent.com/22718430/196056767-d567ec76-650d-4729-8933-e2447bb68244.png
+    imageURL: "https://user-images.githubusercontent.com/22718430/196056767-d567ec76-650d-4729-8933-e2447bb68244.png",
   },
 
   {
     name: "Hailey Luong",
     position: "Social Media Manager",
     instagram: "https://www.instagram.com/haileyxluong/",
-    imageURL: https://user-images.githubusercontent.com/22718430/196056851-97e43aca-e7c6-4b3f-bda3-3e202a939bb6.png
+    imageURL: "https://user-images.githubusercontent.com/22718430/196056851-97e43aca-e7c6-4b3f-bda3-3e202a939bb6.png",
   },
   {
     name: "Aaryahi Vaidya",
@@ -44,19 +45,19 @@ const teamMembers = [
     instagram: “https://www.instagram.com/aari.vaidya/",
     github: “https://github.com/aaryahiv?tab=repositories”,
     linkedin: “https://www.linkedin.com/in/aaryahi-vaidya-110553242”,
-    imageURL: https://user-images.githubusercontent.com/22718430/196056754-3378fc00-2ff4-4339-8a7c-f90078d0eb7f.png
+    imageURL: "https://user-images.githubusercontent.com/22718430/196056754-3378fc00-2ff4-4339-8a7c-f90078d0eb7f.png",
   },
   {
     name: "Paulina Depaulo",
     position: "Finance Officer",
     instagram: “https://www.instagram.com/paulina.maee",
-    imageURL: https://user-images.githubusercontent.com/22718430/196056901-54730b64-c54c-4aca-a01d-e82b49f2bacc.png
+    imageURL: "https://user-images.githubusercontent.com/22718430/196056901-54730b64-c54c-4aca-a01d-e82b49f2bacc.png",
   },
   {
     name: "David Rendon",
     position: "Secretary",
     instagram: “https://www.instagram.com/_a_simple_cactus_/",
-    imageURL: https://user-images.githubusercontent.com/22718430/196056785-9a5b4e81-2ea5-4e9f-ba5b-9192c73eee4b.png
+    imageURL: "https://user-images.githubusercontent.com/22718430/196056785-9a5b4e81-2ea5-4e9f-ba5b-9192c73eee4b.png",
   }
 
 ];

cfbbf955a8ebeac7f4f9eb52e93e67ded17aefe6
Aaryahi Vaidya <aaryahi.v@gmail.com>
2023-04-14 11:34:04 -0700

    Create WritingFest.js

---
+++ src/pages/WritingFest.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useContext } from "react";
+import Hero from "../components/home/Hero";
+
+
+export default function WritingFest() {
+  return (
+    <div className="writingfest page">
+      <SectionHero
+        title="WritingFest"
+        subtitle=""
+        imageURL="https://lh3.googleusercontent.com/drive-viewer/AAOQEORQ85zz9MkbYInz3LDNVt3sVEHsZchlHBK8wYohGsEB5uS_LOfM4aZCm1tccoh1zv7YIf1BkWfRW0USD2t8aFNN5zMFqQ=s1600"
+      />
+      < The Rules />
+    </div>
+  );
+}
